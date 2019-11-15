60b2f8ef54e1fc8b2453d6834a998dc98f67bb82
AMEGLIO Loic <amegliol@hostname.iut2.upmf-grenoble.fr>
2019-11-15 14:36:57 +0100

    commit view

--- view/main.view.php
+++ view/main.view.php
@@ -1,99 +1,4 @@
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="fr" dir="ltr">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-  <title>Scale Echirolles</title>
-</head>
-<style media="screen">
-  footer{
-    position: fixed;
-    bottom: 0px ;
-    width : 100%;
-
-  }
-</style>
-<body>
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-    <a class="navbar-brand" href="#">Scale Echirolles</a>
-    <ul class="navbar-nav">
-
-      <li class="nav-item"><a class="nav-link" href="">Accueil</a></li>
-
-      <li class="nav-item"><a class="nav-link" href="">Les Coureurs</a></li>
-
-      <li class="nav-item"><a class="nav-link" href="">Le Bureau</a></li>
-
-      <li class="nav-item"><a class="nav-link" href="">Le Club</a></li>
-
-      <li class="nav-item"><a class="nav-link" href="">Nos Organisations</a></li>
-
-      <li class="nav-item"><a class="nav-link" href="">Historique 2001-2011</a></li>
-
-      <li class="nav-item"><a class="nav-link" href="">Historique 1981-2001</a></li>
-
-      <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
-
-
-
-      <li class="nav-item"> <a style="color:#FF4500;" class="nav-link" href="../view/admin.view.php">Administrateur</a></li>
-
-    </ul>
-  </nav>
-  <section id="main">
-    <div id="carousel" class="carousel slide" data-ride="carousel">
-      <ol class="carousel-indicators">
-        <li data-target="#carouselIndicators" data-slide-to="0" class="active"></li>
-        <li data-target="#carouselIndicators" data-slide-to="1"></li>
-        <li data-target="#carouselIndicators" data-slide-to="2"></li>
-      </ol>
-
-      <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img class="d-block w-100" src="ob_763433_p1620864.JPG" alt="First slide">
-      <div class="carousel-caption">
-     </div>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="ob_00da8f_l-equipe-scale-2019.jpg" alt="Second slide">
-      <div class="carousel-caption">
-     </div>
-    </div>
-    <div class="carousel-item">
-      <img class="d-block w-100" src="ob_c45056_p1620014.JPG" alt="Third slide">
-      <div class="carousel-caption">
-    </div>
-    </div>
-  </div>
-  <a class="carousel-control-prev" href="#carousel" role="button" data-slide="prev">
-    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="carousel-control-next" href="#carousel" role="button" data-slide="next">
-    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-    </div>
-    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light">
-
-  </section>
-
-
-
-  <footer>
-    <nav nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-      <ul class="navbar-nav">
-        <li class="nav-item"><a class="nav-link" href="#">F.A.Q</a></li>
-      </ul>
-    </nav>
-  </footer>
-
-=======
 <html lang="en" dir="ltr">
 <head>
   <!-- Required meta tags -->
@@ -123,10 +28,10 @@
           <a class="nav-link" href="actualites.view.php">Actualités</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="photos.view.php">Photos</a>
+          <a class="nav-link" href="lescoureurs.view.php">Les coureurs</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="articles.view.php">Nos articles</a>
+          <a class="nav-link" href="histoireclub.view.php">Histoire du club</a>
         </li>
       </ul>
     </div>
@@ -165,7 +70,6 @@
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
->>>>>>> 23555ac9685f91977313980d6a250251fa940597
 
 </body>
 </html>
