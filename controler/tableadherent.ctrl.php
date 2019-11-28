f65561b5be162323aac3dcf5578eec0a180ec0ae
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-11-28 17:11:51 +0100

    test

---
+++ controler/tableadherent.ctrl.php
@@ -0,0 +1,16 @@
+<?php
+require_once('adherent.class.php');
+require_once('adherentDAO.class.php');
+
+// Récupération des données de configuration
+$config = parse_ini_file('../config/config.ini');
+
+// Creation de l'instace DAO
+$adherents = new adherentDAO($config['database_path']);
+
+//$n = $adherents->getUnAdherent(1);
+$nblignes = $adherents->getNbLigne();
+
+//view
+include('../view/adminpage.php');
+?>
--- view/adminpage.php
+++ view/adminpage.php
@@ -33,7 +33,7 @@
               <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
                 <thead>
                   <tr> <!-- remplissage auto avec notre bd -->
-                    <th>Name</th>
+                    <th>Name</th><!-- recuperer nomcolonne avec notre bd -->
                     <th>Position</th>
                     <th>Office</th>
                     <th>Age</th>
@@ -41,41 +41,18 @@
                     <th>Salary</th>
                   </tr>
                 </thead>
-                <tfoot>
-                  <tr>
-                    <th>Name</th>
-                    <th>Position</th>
-                    <th>Office</th>
-                    <th>Age</th>
-                    <th>Start date</th>
-                    <th>Salary</th>
-                  </tr>
-                </tfoot>
                 <tbody>
+                  <?php $i=0; while ($nblignes > 0){ ?>
                   <tr>
-                    <td>Tiger Nixon</td>
+                    <td><?=$adherents->getUnAdherent($i)->getNom(); ?></td>
                     <td>System Architect</td>
                     <td>Edinburgh</td>
                     <td>61</td>
                     <td>2011/04/25</td>
                     <td>$320,800</td>
                   </tr>
-                  <tr>
-                    <td>Garrett Winters</td>
-                    <td>Accountant</td>
-                    <td>Tokyo</td>
-                    <td>63</td>
-                    <td>2011/07/25</td>
-                    <td>$170,750</td>
-                  </tr>
-                  <tr>
-                    <td>Ashton Cox</td>
-                    <td>Junior Technical Author</td>
-                    <td>San Francisco</td>
-                    <td>66</td>
-                    <td>2009/01/12</td>
-                    <td>$86,000</td>
-                  </tr>
+                <?php $nblignes--;
+                      $i++;} ?>
                 </tbody>
               </table>
             </div>
