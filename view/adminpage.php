9156b508d6de907a7cb7cf9093af8bf4ffc0cc33
BERNABE Aurelien <bernabea@hostname.iut2.upmf-grenoble.fr>
2019-11-29 13:28:29 +0100

    adminpage

--- view/adminpage.php
+++ view/adminpage.php
@@ -19,7 +19,7 @@
   <div class="jumbotron text-center" style="margin-bottom:0">
     <img src="../data/images/accueil_banniere.jpg" alt="">
   </div>
-<!-- NAV DE BASE !-->
+  <!-- NAV DE BASE !-->
   <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <a class="navbar-brand" href="main.view.php"> <img src="../data/images/logo-scale.jpg" alt=""></a>
 
@@ -46,49 +46,38 @@
   <!-- NAV DE BASE !-->
 
 
-  <div id="wrapper">
-      <div id="content-wrapper">
-        <!-- DataTables Example -->
-        <div class="card-header">
-          <i class="fas fa-table"></i>
-          Data Table Example</div>
-          <div class="card-body">
-            <div class="table-responsive">
-              <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
-                <thead>
-                  <tr> <!-- remplissage auto avec notre bd -->
-                    <th>idadherent</th><!--  nomcolonne de la bd -->
-                    <th>nom</th>
-                    <th>prenom</th>
-                    <th>categorie</th>
-                    <th>datenaissance</th>
-                    <th>adresse</th>
-                    <th>telephone</th>
-                    <th>mail</th>
-                    <th>numlicence</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <?php $i=1; while ($nblignes > 0){ ?>
-                  <tr>
-                    <td><?=$adherents->getUnAdherent($i)->getIdAdherent();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getNom();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getPrenom();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getCategorie();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getDateNaissance();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getAdresse();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getTelephone();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getMail();?></td>
-                    <td><?=$adherents->getUnAdherent($i)->getNumLicence();?></td>
-                  </tr>
-                <?php $nblignes--;
-                      $i++;} ?>
-                </tbody>
-              </table>
-            </div>
-          </div>
-        </div>
-      </div>
-      <!-- /.content-wrapper -->
-    </body>
-    </html>
+  <!-- Tableau !-->
+  <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
+    <thead>
+      <tr> <!-- remplissage auto avec notre bd -->
+        <th>idadherent</th><!--  nomcolonne de la bd -->
+        <th>nom</th>
+        <th>prenom</th>
+        <th>categorie</th>
+        <th>datenaissance</th>
+        <th>adresse</th>
+        <th>telephone</th>
+        <th>mail</th>
+        <th>numlicence</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php $i=1; while ($nblignes > 0){ ?>
+        <tr>
+          <td><?=$adherents->getUnAdherent($i)->getIdAdherent();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getNom();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getPrenom();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getCategorie();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getDateNaissance();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getAdresse();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getTelephone();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getMail();?></td>
+          <td><?=$adherents->getUnAdherent($i)->getNumLicence();?></td>
+        </tr>
+        <?php $nblignes--;
+        $i++;} ?>
+      </tbody>
+    </table>
+    <!-- Fin tableau -->
+  </body>
+  </html>
