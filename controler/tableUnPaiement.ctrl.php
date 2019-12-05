80b3d42b33d0ad38f5d34b9faf58b7010528012d
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-05 10:44:58 +0100

    aze

--- controler/tableUnPaiement.ctrl.php
+++ controler/tableUnPaiement.ctrl.php
@@ -5,7 +5,7 @@ require_once('../model/classPaiement/paiementDAO.class.php');
 // Creation de l'instance DAO
 $paiements = new paiementDAO('../model/data/db');
 
-if (isset($_GET['idAdherent']) && $paiements->paiementExiste()!= 0 ){
+if (isset($_GET['idAdherent']) && ($paiements->paiementExiste($_GET['idAdherent']) != 0) ){
   $id=$_GET['idAdherent'];
   $respaiement = $paiements->getUnPaiement($id);
 }else{
