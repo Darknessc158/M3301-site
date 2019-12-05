86074ecc3648adf306f8b6943bafcfecb97cb7d7
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-05 11:16:08 +0100

    azeeza

--- controler/tableUnPaiement.ctrl.php
+++ controler/tableUnPaiement.ctrl.php
@@ -8,6 +8,8 @@ $paiements = new paiementDAO('../model/data/db');
 if (isset($_GET['idAdherent']) && ($paiements->paiementExiste($_GET['idAdherent']) != 0) ){
   $id=$_GET['idAdherent'];
   $res = $paiements->getUnPaiement($id);
+}else if(isset($_GET['idAdherent'])){
+  $res = "Pas de paiement pour cette adherent"
 }else{
   $res = $paiements->getLesPaiements();
 }
