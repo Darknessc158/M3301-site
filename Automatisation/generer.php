01d149cdb5550859a91b883b83d00c0b0920187d
REMULE Fabien <remulef@hostname.iut2.upmf-grenoble.fr>
2019-12-06 14:40:45 +0100

    doc

--- Automatisation/generer.php
+++ Automatisation/generer.php
@@ -13,7 +13,7 @@ $fields = array(
 	'ADRESSE' => '12345'
 );
 
-$pdf = new FPDM('document/doc_faitmain/Sanstitre.pdf');
+$pdf = new FPDM('document/doc_faitmain/compatible.pdf');
 $pdf->Load($fields, false); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
 $pdf->Merge();
 $pdf->Output("D", "formulaire_remplie.pdf");
