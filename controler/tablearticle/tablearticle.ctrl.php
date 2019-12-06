6cd44f8b4885fa98f03dd8fc4f810eafa9287a59
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-06 09:07:51 +0100

    tablearticle ...

---
+++ controler/tablearticle/tablearticle.ctrl.php
@@ -0,0 +1,4 @@
+<!-- A faire : Afficher les articles pour un adherent requete imbriquée avec la class paiement
+  Faire ça pour licence et adhesion aussi peut etre pour que dans paiement on est que la date et le type puis apres
+  redirection vers classe article,licence et adhesion pour plus de précision.
+-->
