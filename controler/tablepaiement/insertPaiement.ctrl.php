550eb10094ddc152fcc6026bc25548661515bbda
BERNABE Aurelien <bernabea@hostname.iut2.upmf-grenoble.fr>
2019-12-07 10:45:13 +0100

    azeaze

--- controler/tablepaiement/insertPaiement.ctrl.php
+++ controler/tablepaiement/insertPaiement.ctrl.php
@@ -17,7 +17,7 @@ $prix = $_POST['prix'];
 $description = $_POST['description'];
 $etatDuPaiement = $_POST['etatDuPaiement'];
 $type = $_POST['type'];
-if (isset['idAdherent']){
+if (isset($_GET['idAdherent'])){
   $idAdherent = $_GET['idAdherent'];
 }else{
 $idAdherent = $_POST['idAdherent'];
@@ -25,14 +25,26 @@ $idAdherent = $_POST['idAdherent'];
 $unPaiement = $paiements->insertUnPaiement($datePaiement,$prix,$description,$etatDuPaiement,$type,$idAdherent);
 
 
-$res = $paiements->getLesPaiements();
-$respaiement = $res;
+//GESTION affichage paiement----------------------------------------
+if (isset($_GET['idAdherent'])){ // on a l'id adherent
+  $id=$_GET['idAdherent'];
+  $res = $paiements->getLesPaiementsDunAdherent($id);
+}else{
+  $res = $paiements->getLesPaiements(); // si pas id on recupere tous les paiements
+}
+$respaiement = $res; //on remplit la variable pour la view
+//--------------------------------------------------------------------
+
 
 //recuperation des donnes pour recup le nom et prenom de l'id qui correspond
 require_once('../../model/classAdherent/adherent.class.php');
 require_once('../../model/classAdherent/adherentDAO.class.php');
 $config = parse_ini_file('../../config/config.ini');
 $adherents = new adherentDAO($config['database_path']);
+$lesadh = $adherents->getLesAdherents();
 
+//----------------------------------------------------------
+
+//view
 include('../../view/paiementview/adminpage.paiement.php');
 ?>
