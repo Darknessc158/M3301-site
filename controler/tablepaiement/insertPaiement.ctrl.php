dca44ae5576fc6784999682d7f986907b43e0216
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-07 09:23:50 +0100

    Update insertPaiement.ctrl.php
    aze

--- controler/tablepaiement/insertPaiement.ctrl.php
+++ controler/tablepaiement/insertPaiement.ctrl.php
@@ -1,28 +1,23 @@
 <?php
-require_once('../../model/classPaiement/paiement.class.php');
-require_once('../../model/classPaiement/paiementDAO.class.php');
+require_once('../../model/classArticle/article.class.php');
+require_once('../../model/classArticle/articleDAO.class.php');
 
-// Creation de l'instance DAO
-$paiements = new paiementDAO('../../model/data/db');
-
-
-$datePaiement = $_POST['datePaiement'];
-$prix = $_POST['prix'];
-$description = $_POST['description'];
-$etatDuPaiement = $_POST['etatDuPaiement'];
-$type = $_POST['type'];
-$idAdherent = $_POST['idAdherent'];
-
-$unPaiement = $paiements->insertUnPaiement($datePaiement,$prix,$description,$etatDuPaiement,$type,$idAdherent);
-
-$res = $paiements->getLesPaiements();
-$respaiement = $res;
-
-//recuperation des donnes pour recup le nom et prenom de l'id qui correspond
 require_once('../../model/classAdherent/adherent.class.php');
 require_once('../../model/classAdherent/adherentDAO.class.php');
+
+// Récupération des données de configuration
 $config = parse_ini_file('../../config/config.ini');
+
+
+$articles = new articleDAO($config['database_path']);
 $adherents = new adherentDAO($config['database_path']);
 
-include('../../view/paiementview/adminpage.paiement.php');
-?>
+$lesadh = $adherents->getLesAdherents();//recuperation des adhérents
+$lesArticles = $articles->getLesArticles(); //recuperation des articles
+
+//view
+include('../../view/paiementview/insertPaiement.php');
+
+
+
+ ?>
