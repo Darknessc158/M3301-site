16dbed344deaca8037158f2455b58ecebbe2d0e9
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-07 10:08:22 +0100

    insertPaiement.ctrl

---
+++ controler/tablepaiement/insertPaiement.ctrl.php
@@ -0,0 +1,35 @@
+<?php
+require_once('../../model/classPaiement/paiement.class.php');
+require_once('../../model/classPaiement/paiementDAO.class.php');
+require_once('../../model/classArticle/article.class.php');
+require_once('../../model/classArticle/articleDAO.class.php');
+
+// Creation de l'instance DAO
+$paiements = new paiementDAO('../../model/data/db');
+require_once('../../model/classAdherent/adherent.class.php');
+require_once('../../model/classAdherent/adherentDAO.class.php');
+
+// Récupération des données de configuration
+$config = parse_ini_file('../../config/config.ini');
+
+$datePaiement = $_POST['datePaiement'];
+$prix = $_POST['prix'];
+$description = $_POST['description'];
+$etatDuPaiement = $_POST['etatDuPaiement'];
+$type = $_POST['type'];
+$idAdherent = $_POST['idAdherent'];
+
+$unPaiement = $paiements->insertUnPaiement($datePaiement,$prix,$description,$etatDuPaiement,$type,$idAdherent);
+
+
+$res = $paiements->getLesPaiements();
+$respaiement = $res;
+
+//recuperation des donnes pour recup le nom et prenom de l'id qui correspond
+require_once('../../model/classAdherent/adherent.class.php');
+require_once('../../model/classAdherent/adherentDAO.class.php');
+$config = parse_ini_file('../../config/config.ini');
+$adherents = new adherentDAO($config['database_path']);
+
+include('../../view/paiementview/adminpage.paiement.php');
+?>
