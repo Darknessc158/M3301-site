23d5d35ac263aa256e4f33b0b5f71b8a13aa38f5
REMULE Fabien <remulef@hostname.iut2.upmf-grenoble.fr>
2019-12-12 13:53:26 +0100

    facture

--- Automatisation/facture.php
+++ Automatisation/facture.php
@@ -17,11 +17,7 @@ function facture(int numero_paiement){
 
 
 
-  Classe article&quantite{
- public  $num_article;
- public  $quantite;
 
-  }
   //Liste des numero d'article et des quantite dans une ligne commande
   $req = "SELECT * FROM LIGNE_COMMANDE_ARTICLE  where COMMANDEnum_commande = $numcommande ;";
   $sth=$this->db->query($req);
@@ -31,46 +27,222 @@ function facture(int numero_paiement){
 
    //pour chaque num article de ligne de commande on va
 
+    //Ouverture d'un fichier pour
+    $text= fopen('temp_facture.txt', 'r+');
+
     //Boucle permettant d'afficher tout le produit contenue dans une commandes
     foreach ($listeprod_lignecommande) {
+
       $req = "SELECT * FROM LIGNE_COMMANDE_ARTICLE  where Num_article = $listeprod_lignecommande.ArticleNum_article ;";
       $sth=$this->db->query($req);
       $produit_courant = $sth->fetchAll(PDO::FETCH_CLASS,'article');
 
 
       //Il faudra mettre ça dans une ligne ou ecrire dans un fichier et utiliser le tableau de fpdm   http://www.fpdf.org/?lang=fr
-      printf("quantité =%s
-              num produit = %s
-              nom article = %s
-              Prix unitaire HT = %d
-              Marque = %s
-              Prix total HT = %d
-              Prix total TTC = %d",$listeprod_lignecommande.quantite,%$listeprod_lignecommande.ARTICLENum_article,%$produit_courant.Description,%$produit_courant.Prix,... );
 
-    //Passer a la ligne
+              $quantite=$listeprod_lignecommande.quantite;
+              $numarticle=$listeprod_lignecommande.ARTICLENum_article;
+              $Description=$produit_courant.Description;
+               $PrixUnitaire=$produit_courant.PrixUnitaireHT;
+              $Marque =$produit_courant.Marque;
+              $PrixTHC=$listeprod_lignecommande.quantite*$produit_courant.PrixUnitaireHT;
+              $PrixTTC=$listeprod_lignecommande.quantite*$produit_courant.PrixUnitaireHT*1.20);
+
+
+    $ligne = $quantite+';'+$numarticle+';'+$Description+';'+$Marque+';'+$PrixUnitaire+';'+$PrixTHC+';'+$PrixTTC+';';
+    fwrite($text,$ligne);
     }
+    //Fermeture du fichier
+    fclose($text);
 
+require('fpdm/fpdm.php');
 
-  require('fpdm/fpdm.php');
+class PDF extends FPDF
+{
+// Chargement des données$ligne = $quantite+';'+$numarticle+';'+$Description+';'+$Marque+';'+$PrixUnitaire+';'+$PrixTHC+';'+$PrixTTC+';';
+function LoadData($file)
+{
+    // Lecture des lignes du fichier
+    $lines = file($file);
+    $data = array();
+    foreach($lines as $line)
+        $data[] = explode(';',trim($line));
+    return $data;
+}
 
+// Tableau simple
+function BasicTable($header, $data)
+{
+    // En-tête
+    foreach($header as $col)
+        $this->Cell(40,7,$col,1);
+    $this->Ln();
+    // Données
+    foreach($data as $row)
+    {
+        foreach($row as $col)
+            $this->Cell(40,6,$col,1);
+        $this->Ln();
+    }
+}
+
+// Tableau amélioré
+function ImprovedTable($header, $data)
+{
+    // Largeurs des colonnes
+    $w = array(40, 35, 45, 40);
+    // En-tête
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C');
+    $this->Ln();
+    // Données
+    foreach($data as $row)
+    {
+        $this->Cell($w[0],6,$row[0],'LR');
+        $this->Cell($w[1],6,$row[1],'LR');
+        $this->Cell($w[2],6,number_format($row[2],0,',',' '),'LR',0,'R');
+        $this->Cell($w[3],6,number_format($row[3],0,',',' '),'LR',0,'R');
+        $this->Ln();
+    }
+    // Trait de terminaison
+    $this->Cell(array_sum($w),0,'','T');
+}
+
+// Tableau coloré
+function FancyTable($header, $data)
+{
+    // Couleurs, épaisseur du trait et police grasse
+    $this->SetFillColor(255,0,0);
+    $this->SetTextColor(255);
+    $this->SetDrawColor(128,0,0);
+    $this->SetLineWidth(.3);
+    $this->SetFont('','B');
+    // En-tête
+    $w = array(40, 35, 45, 40);
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+    $this->Ln();
+    // Restauration des couleurs et de la police
+    $this->SetFillColor(224,235,255);class PDF extends FPDF
+{
+// Chargement des données
+function LoadData($file)
+{
+    // Lecture des lignes du fichier
+    $lines = file($file);
+    $data = array();
+    foreach($lines as $line)
+        $data[] = explode(';',trim($line));
+    return $data;
+}
+
+// Tableau simple
+function BasicTable($header, $data)
+{
+    // En-tête
+    foreach($header as $col)
+        $this->Cell(40,7,$col,1);
+    $this->Ln();
+    // Données
+    foreach($data as $row)
+    {
+        foreach($row as $col)
+            $this->Cell(40,6,$col,1);
+        $this->Ln();
+    }
+}
+
+// Tableau amélioré
+function ImprovedTable($header, $data)
+{
+    // Largeurs des colonnes
+    $w = array(40, 35, 45, 40);
+    // En-tête
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C');
+    $this->Ln();
+    // Données
+    foreach($data as $row)
+    {
+        $this->Cell($w[0],6,$row[0],'LR');
+        $this->Cell($w[1],6,$row[1],'LR');
+        $this->Cell($w[2],6,number_format($row[2],0,',',' '),'LR',0,'R');
+        $this->Cell($w[3],6,number_format($row[3],0,',',' '),'LR',0,'R');
+        $this->Ln();
+    }
+    // Trait de terminaison
+    $this->Cell(array_sum($w),0,'','T');
+}
+
+// Tableau coloré
+function FancyTable($header, $data)
+{
+    // Couleurs, épaisseur du trait et police grasse
+    $this->SetFillColor(255,0,0);
+    $this->SetTextColor(255);
+    $this->SetDrawColor(128,0,0);
+    $this->SetLineWidth(.3);
+    $this->SetFont('','B');
+    // En-tête
+    $w = array(40, 35, 45, 40);
+    for($i=0;$i<count($header);$i++)
+        $this->Cell($w[$i],7,$header[$i],1,0,'C',true);
+    $this->Ln();
+    // Restauration des couleurs et de la police
+    $this->SetFillColor(224,235,255);
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Données
+    $fill = false;
+    foreach($data as $row)
+    {
+        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+        $this->Cell($w[2],6,number_format($row[2],0,',',' '),'LR',0,'R',$fill);
+        $this->Cell($w[3],6,number_format($row[3],0,',',' '),'LR',0,'R',$fill);
+        $this->Ln();
+        $fill = !$fill;
+    }
+    // Trait de terminaison
+    $this->Cell(array_sum($w),0,'','T');
+}
+}
+    $this->SetTextColor(0);
+    $this->SetFont('');
+    // Données
+    $fill = false;
+    foreach($data as $row)
+    {
+        $this->Cell($w[0],6,$row[0],'LR',0,'L',$fill);
+        $this->Cell($w[1],6,$row[1],'LR',0,'L',$fill);
+        $this->Cell($w[2],6,number_format($row[2],0,',',' '),'LR',0,'R',$fill);
+        $this->Cell($w[3],6,number_format($row[3],0,',',' '),'LR',0,'R',$fill);
+        $this->Ln();
+        $fill = !$fill;
+    }
+    // Trait de terminaison
+    $this->Cell(array_sum($w),0,'','T');
+}
+}
 
   $fields = array(
+    //Haut de la facture
   	'num_paiement'    => $numPaiement,
   	'num_commande' => $numcommande,
   	'nom_client' => $membre.nom,
   	'adresse_client' => $membre.adress,
-    'ligne1_quantite' =>,
-    'ligne1_numarticle' =>,
-    'ligne1_nom' => ,
-    'marque' => ,
-    'prix' =>
+
   );
 
-  $pdf = new FPDM('doc_compa.pdf');
+  $pdf = new FPDM('doc_compta.pdf');
+  $header = ('Quantite','Num Article','Description','Marque','Prix UTC','PrixTHC','PrixTTC');
+  $data = $pdf->LoadData('temp_facture.txt');
+  $pdf->AddPage();
+  $pdf->BasicTable($header,$data);
   $pdf->Load($fields, false); // second parameter: false if field values are in ISO-8859-1, true if UTF-8
   $pdf->Merge();
-  $pdf->Output("D", "formulaire_remplie.pdf");
+  $pdf->Output();
 
-
+}
 
  ?>
