e31dddaf702048b9edac2963fe7dde6eb551f6c0
REMULE Fabien <remulef@hostname.iut2.upmf-grenoble.fr>
2019-12-15 19:25:34 +0100

    a

--- model/fpdm/ex-array.php
+++ model/fpdm/ex-array.php
@@ -5,32 +5,13 @@
 ****************************/
 
 require('fpdm.php');
+if(isset($_POST['valider'])){
 
-$name = $_POST['name']);
-$address = $_POST['address']);
-$city = $_POST['city']);
-$phone = $_POST['phone']);
-
-
-	if (!isset($name)) {
-		 	$phone = "test";
-	}
-	if (!isset($address)) {
-			$phone = "test";
-	}
-	if (!isset($city)) {
-		 	$phone = "test";
-	}
-	if (!isset($phone)) {
-		$phone = "test";
-	}
-
-	echo "oui";
 	$fields = array(
-		'name'    => $name,
-		'address' => $address,
-		'city'    => $city,
-		'phone'   =>  $phone
+		'name'    => $_POST['name'],
+		'address' =>  $_POST['address'],
+		'city'    =>  $_POST['city'],
+		'phone'   =>  $_POST['phone']
 	);
 
 	$pdf = new FPDM('template.pdf');
