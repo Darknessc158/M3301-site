460b57d13c1d274e7e5978e85cf9f824212e162e
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-18 16:43:46 +0100

    ad

--- model/data/db/adherents.sql
+++ model/data/db/adherents.sql
@@ -5,7 +5,7 @@ INSERT INTO adherent (nom,prenom,categorie,datenaissance,adresse,telephone,mail,
 VALUES('POLYDORAS','Dimitri','Ufolep 3','25/09/2000','La mure','0764879523','dimitri.polydoras@hotmail.fr','035894684',null);
 
 INSERT INTO adherent (nom,prenom,categorie,datenaissance,adresse,telephone,mail,numLicence,urlPhoto)
-VALUES('BERNABE','Aurélien','Ufolep 2','10/08/2002','Pierre chatel','0664879523','aurelien.bernabe@hotmail.fr','0358946458',null);
+VALUES('BERNABE','Aurélien','Ufolep 2','10/08/2002','Pierre chatel','0664879523','aurelien.bernabe@hotmail.fr','0358946458','../../model/data/images/lescoureurs/aurelien-bernabe.jpg');
 
 INSERT INTO adherent (nom,prenom,categorie,datenaissance,adresse,telephone,mail,numLicence,urlPhoto)
 VALUES('AMEGLIO','Loic','Ufolep 2','18/09/2001','Marseille','0664879523','loic.ameglio@free.fr','034877223',null);
