b8467f572489dda074d6409402eb3b6cb1e002e6
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-18 15:24:08 +0100

    accueil ++

--- view/autres/accueil.view.php
+++ view/autres/accueil.view.php
@@ -5,22 +5,24 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-  <link href="../../view/css/main.css" rel="stylesheet">
+  <link href="../../view/css/main.view.css" rel="stylesheet">
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   <title>SCALE Echirolles - club de cyclisme</title>
 </head>
-<body style="background-color:silver;">
+<body style="background-color: #FFFFEE;">
 
-  <div class="boximg" style="margin-bottom:0">
+<header>
+  <!--Image bandeau haut-->
+  <div class="boximg">
     <p class="box">
       <img class="scroll" src="../../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
       <img class="scroll" src="../../model/data/images/images_sites/fond_banniere.jpg" alt="" style="display:none;">
       <img class="scroll" src="../../model/data/images/images_sites/ob_763433_p1620864.jpg" alt="" style="display:none;">
     </p>
   </div>
-
+  <!--Script defilement voir après.
   <script type="text/javascript">
   I = 0 ;
   Imax = document.images.length - 1 ;
@@ -36,14 +38,15 @@
     setTimeout(suivante, 7000) ;
   }
   </script>
-
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="display:flex; flex-wrap:wrap;">
+  <!--Image bandeau haut-->
+  <!--Nav-->
+  <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
     <a class="navbar-brand" href="#"> <img src="../../model/data/images/images_sites/logo-scale.jpg" alt="" width="60%"></a>
 
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="main.view.php">Accueil</a>
+          <a class="nav-link" href="accueil.view.php">Accueil</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="actualites.view.php">Actualités</a>
@@ -66,28 +69,57 @@
       </ul>
     </div>
   </nav>
+  <!--Nav-->
+</header>
+<!--Fin header-->
+<div class="all">
+
+
+
+<div class="droite">
+  <div class="infos">
+    <h5>Informations pratiques</h5>
+    <p>Adhésion au SCALE : 30 € échirollois - 40 € extérieurs.</p>
+    <p>Licence suivant l'âge.</p>
+    <p>Réunion les 1ers mardis de chaque mois à 20 h au siège du S.C.A.L.E.,
+      Gymnase Auguste Delaune, 9 rue Manouchian - 38130 Echirolles.</p>
+    </p>
+  </div>
+  <div class="sponsors">
+    <h5>Nos sponsors</h5>
+    <a href="https://piecesaccessoiresvelo.fr/">
+    <img src="../../model/data/images/images_sites/sponsor_pieces.png" alt="" width="60%">
+    </a>
+    <a href="https://www.gites-de-france-isere.com/">
+    <img src="../../model/data/images/images_sites/sponsor_gite.jpg" alt="" width="50%">
+    </a>
+  </div>
+  <div class="localisation">
+    <h5>Retrouvez-nous !</h5>
+    <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1AknAsXQlGn_Si6Ydd5zXAqj6hp_mXRe-" height="280" style="max-width:98%"></iframe>
+  </div>
+</div>
+
+<div class="gauche">
+  
+</div>
+
+
+<div class="centergauche">
+
 
 
   <h1>Rejoignez-nous ! </h1>
   <div class="container">
-    <div class="element">
+    <div class="elementqsn">
       <h3>Qui sommes nous ?</h3>
-      <p>Scale Echirolles est une association sportive de cyclisme.
-        Nous sommes une vingtaine d'adhérents composés de 17 coureurs.
-        Si vous aimez le cyclisme (se faire plaisir en roulant avec des amis) alors n'attendez plus SCALE Echirolles est fait
-        pour vous ! Venez nous rejoindre le week-end ou même le mercredi après-midi pour rouler en petit comité.
-        Vous pourrez aussi participer à plusieurs courses dont notamment celles organisées par le club sous l'égide de l'UFOLEP
-        ainsi que de la FFC.
-      </p>
-    </div>
-    <div class="element">
-      <h3>La vie du club</h3>
-      <h4>Informations pratiques</h4>
-      <p>Adhésion au SCALE : 30 € échirollois - 40 € extérieurs.</p>
-      <p>Licence suivant l'âge.</p>
-      <p>Réunion les 1ers mardis de chaque mois à 20 h au siège du S.C.A.L.E.,
-        Gymnase Auguste Delaune, 9 rue Manouchian - 38130 Echirolles.</p>
-      </p>
+      <p>Scale Echirolles est une association sportive de cyclisme.</p>
+      <p>Nous sommes une vingtaine d'adhérents composés de 17 coureurs.</p>
+      <p>Si vous aimez le cyclisme et encore plus accompagné alors n'attendez plus SCALE Echirolles est fait
+      pour vous ! <i class="far fa-smile-wink"></i></p>
+      <p>Venez nous rejoindre le week-end ou même le mercredi après-midi pour rouler en petit comité.
+      Vous pourrez aussi participer à plusieurs courses dont notamment celles organisées par le club sous l'égide de l'UFOLEP
+      ainsi que de la FFC.</p>
     </div>
   </div>
 
@@ -104,12 +136,6 @@
       <p>Toutes les sorties et activités seront encadrées par des éducateurs et entraîneurs diplômés.</p>
     </div>
 
-    <div class="elemententrainement">
-      <h4>Retrouvez-nous !</h4>
-      <img src="../../model/data/images/accueil/entrainement_depart.jfif" alt="" style="max-width:99%; height:auto; margin-bottom:10px; vertical-align:top;" >
-      <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1AknAsXQlGn_Si6Ydd5zXAqj6hp_mXRe-" width="230" height="170" style="border-radius:15px;"></iframe>
-    </div>
-
     <div class="element">
       <h3>Les types d'activités</h3>
       <p>Epreuves de maniabilité et séances de PPG dans le gymnase (hiver)</p>
@@ -118,21 +144,26 @@
       <p>Sortie de club (retrouvailles de tous les coureurs)</p>
     </div>
   </div>
+  <div class="container">
+    <img src="../../model/data/images/accueil/entrainement_depart2.jfif" alt="" title="Départ Joliot Curie" width="65%" height="auto">
+  </div>
+
+</div><!--Fin des deux div gauche et droite-->
 
   <p id="separator">--------------------------------------------------------------------------------------</p>
   <h3 style="text-align:center; margin-bottom: 20px;">Les stages</h3>
-    <div class="container" style="border: 1px solid gray; border-radius: 20px;">
+    <div class="containerstage">
       <h5>Stage de cohésion (un peu de vélo, beaucoup d'amusement !)</h5>
       <div class="container">
-      <img src="../../model/data/images/accueil/stage1_cohesion1.JPG" alt="" style="max-width:40%; height:auto; margin-bottom:10px;">
-      <img src="../../model/data/images/accueil/stage1_cohesion2.JPG" alt="" style="max-width:40%; height:auto; margin-bottom:10px;">
-      <img src="../../model/data/images/accueil/stage1_depart.JPG" alt="" style="max-width:40%; height:auto; margin-bottom:10px;">
+      <img src="../../model/data/images/accueil/stage1_cohesion1.JPG" alt="" title="La Robine" style="max-width:40%; height:auto; margin-bottom:10px;">
+      <img src="../../model/data/images/accueil/stage1_cohesion2.JPG" alt="" title="La Robine" style="max-width:40%; height:auto; margin-bottom:10px;">
+      <img src="../../model/data/images/accueil/stage1_depart.JPG" alt="" title="La Robine" style="max-width:40%; height:auto; margin-bottom:10px;">
       </div>
     </div>
-    <div class="container" style="border: 1px solid gray; border-radius: 20px;  margin-bottom: 20px;">
+    <div class="containerstage">
       <h5>Stage de présaison (Un peu plus de vélo et avec plaisir !)</h5>
       <div class="container">
-      <img src="../../model/data/images/accueil/stage2_depart.JPG" alt="" style="max-width:40%; height:auto; margin-bottom:10px;">
+      <img src="../../model/data/images/accueil/stage2_depart.JPG" alt="" title="Veyne" style="max-width:40%; height:auto; margin-bottom:10px;">
       <img src="../../model/data/images/accueil/stage2_noyer.JPG" alt="" title="Col du noyer" style="max-width:60%; height:auto;">
       </div>
       <p>Le stage de présaison est là pour peaufiner l'entrainement du début de saison afin d'atteindre un bon niveau pour
@@ -144,9 +175,10 @@
   <div class="container">
     <div class="element">
       <h3>Les dates clefs du club</h3>
-      <p>Assemblée Générale du SCALE : 17/11/18 </p>
-      <p>Loto du SCALE : 08/12/18 </p>
-      <p>Tirage des rois et signature des licences : 11/01/19 </p>
+      <p>Assemblée Générale du SCALE : mi-novembre </p>
+      <p>Loto du SCALE : début-décembre </p>
+      <img src="../../model/data/images/accueil/loto.JPG" alt="" width="50%">
+      <p>Tirage des rois et signature des licences : début-janvier </p>
     </div>
     <div class="element">
       <h3>Les courses organisées par le club</h3>
@@ -156,7 +188,7 @@
   </div>
 
   <p id="separator">--------------------------------------------------------------------------------------</p>
-  <h2>Quelques chiffres du SCALE (STRAVA)</h2>
+  <h2>Quelques chiffres du SCALE</h2>
   <div class="container">
     <div class="elementstats">
       <h3>Victoires ...</h3>
@@ -172,19 +204,18 @@
     </div>
   </div>
 
-    <p id="separator">--------------------------------------------------------------------------------------</p>
-    <h2>Retrouvez ici nos sponsors :</h2>
-    <div class="container">
-      <div class="element">
-        <h3>Nos sponsors</h3>
-        <p>Retrouvez ici nos sponsors :</p>
-        <img src="../../model/data/images/images_sites/sponsor_gite.jpg" alt="" width="30%">
-        <img src="../../model/data/images/images_sites/sponsor_pieces.png" alt="" width="30%">
-      </div>
-    </div>
+  <p id="separator">--------------------------------------------------------------------------------------</p>
+  <div class="container" style="background-color:white;">
+    <h2>Planning entrainements et courses</h2>
+    <iframe src="https://calendar.google.com/calendar/embed?src=bernabeaurelien%40gmail.com&ctz=Europe%2FParis" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
+  </div>
 
 
 
+
+
+
+</div>
 
 
 
@@ -195,5 +226,6 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
   </body>
   </html>
