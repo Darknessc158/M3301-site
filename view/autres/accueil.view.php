28ba9110cb4c855e19a85d39b4dbeca8c65dc0df
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-19 14:10:55 +0100

    link

--- view/autres/accueil.view.php
+++ view/autres/accueil.view.php
@@ -47,7 +47,7 @@
     <div class="collapse navbar-collapse" id="collapsibleNavbar">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link" href="accueil.view.php">Accueil</a>
+          <a class="nav-link" href="../controler/lescoureurs.ctrl.php">Accueil</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="actualites.view.php">Actualités</a>
