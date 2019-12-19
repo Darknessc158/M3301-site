5681889003d9b16f6d08f97aa4a7e32ca07571c8
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-19 16:43:50 +0100

    alerte article start

--- controler/auth/profilAdmin.ctrl.php
+++ controler/auth/profilAdmin.ctrl.php
@@ -5,5 +5,15 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 
+//REcuperation des infos sur les stocks des articles
+require_once('../../model/classArticle/article.class.php');
+require_once('../../model/classArticle/articleDAO.class.php');
+
+// Récupération des données de configuration
+$config = parse_ini_file('../../config/config.ini');
+// Creation de l'instance DAO
+$articles = new articleDAO($config['database_path']);
+$lesarticles = $articles->getLesArticles();
+
 include('../../view/page administrateur/admin.view.php');
 ?>
--- view/page administrateur/admin.view.php
+++ view/page administrateur/admin.view.php
@@ -16,7 +16,7 @@
 
   <div class="boximg">
     <p class="box">
-    <img class="scroll" src="../../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
+      <img class="scroll" src="../../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
     </p>
   </div>
 
@@ -35,64 +35,82 @@
   <!-- NAV DE BASE !-->
 
   <div class="page-header  text-center">
-      <h1>Bonjour, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenue.</h1>
+    <h1>Bonjour, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Bienvenue.</h1>
   </div>
   <div class=" text-center">
-      <a href="resetmdp.ctrl.php" class="btn btn-warning">Changer votre mot de passe</a>
-      <a href="logout.ctrl.php" class="btn btn-danger">Se deconnecter</a>
+    <a href="resetmdp.ctrl.php" class="btn btn-warning">Changer votre mot de passe</a>
+    <a href="logout.ctrl.php" class="btn btn-danger">Se deconnecter</a>
   </div>
 
 
-  <div class="row">
+  <div class="row" style="width:100%; margin: auto; display:flex;">
     <div class="col-sm-3">
       <div class="card" style="width: 18rem;">
-      <img class="card-img-top" src="../../model/data/images/images_sites/admin-icon.png" alt="image administrateur">
-      <div class="card-body">
-        <h5 class="card-title">Gestionnaire des adhérents</h5>
-        <p class="card-text">Liste des adhérents, transactions d'un adhérent, modifier ou supprimer un adhérent</p>
-        <a href="../../controler/tableadherent/tableadherent.ctrl.php" class="btn btn-primary">Gérer les adhérents</a>
+        <img class="card-img-top" src="../../model/data/images/images_sites/admin-icon.png" alt="image administrateur">
+        <div class="card-body">
+          <h5 class="card-title">Gestionnaire des adhérents</h5>
+          <p class="card-text">Liste des adhérents, transactions d'un adhérent, modifier ou supprimer un adhérent</p>
+          <a href="../../controler/tableadherent/tableadherent.ctrl.php" class="btn btn-primary">Gérer les adhérents</a>
+        </div>
       </div>
     </div>
+
+    <div class="col-sm-3">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../../model/data/images/images_sites/commandes-icon.png" alt="image gestion des stocks">
+        <div class="card-body">
+          <h5 class="card-title">Gestionnaire des stocks</h5>
+          <p class="card-text">Liste des articles et des quantités, modifier ou supprimer un article</p>
+          <a href="../../controler/tablearticle/tablearticle.ctrl.php" class="btn btn-primary">Gérer les stocks</a>
+        </div>
+      </div>
     </div>
 
-<div class="col-sm-3">
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="../../model/data/images/images_sites/commandes-icon.png" alt="image gestion des stocks">
-  <div class="card-body">
-    <h5 class="card-title">Gestionnaire des stocks</h5>
-    <p class="card-text">Liste des articles et des quantités, modifier ou supprimer un article</p>
-    <a href="../../controler/tablearticle/tablearticle.ctrl.php" class="btn btn-primary">Gérer les stocks</a>
-  </div>
-  </div>
-</div>
-
-<div class="col-sm-3">
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="../../model/data/images/images_sites/transactions-icon.png" alt="image gestion des stocks">
-  <div class="card-body">
-    <h5 class="card-title">Gestionnaire des transactions</h5>
-    <p class="card-text">Liste des transactions, modifier ou supprimer une transaction</p>
-    <a href="../../controler/tablepaiement/tableUnPaiement.ctrl.php" class="btn btn-primary">Gérer les transactions</a>
-  </div>
-  </div>
-</div>
-
-<div class="col-sm-3">
-  <div class="card" style="width: 18rem;">
-  <img class="card-img-top" src="../../model/data/images/images_sites/transactions-icon.png" alt="image gestion des stocks">
-  <div class="card-body">
-    <h5 class="card-title">Gestionnaire du site</h5>
-    <p class="card-text">Modifier le site, Ajouter des articles pour l'actualité ...</p>
-    <a href="#" class="btn btn-primary">Gérer le site</a>
-  </div>
-  </div>
-</div>
-
-
-
-
-
+    <div class="col-sm-3">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../../model/data/images/images_sites/transactions-icon.png" alt="image gestion des stocks">
+        <div class="card-body">
+          <h5 class="card-title">Gestionnaire des transactions</h5>
+          <p class="card-text">Liste des transactions, modifier ou supprimer une transaction</p>
+          <a href="../../controler/tablepaiement/tableUnPaiement.ctrl.php" class="btn btn-primary">Gérer les transactions</a>
+        </div>
+      </div>
     </div>
+
+    <div class="col-sm-3">
+      <div class="card" style="width: 18rem;">
+        <img class="card-img-top" src="../../model/data/images/images_sites/transactions-icon.png" alt="image gestion des stocks">
+        <div class="card-body">
+          <h5 class="card-title">Gestionnaire du site</h5>
+          <p class="card-text">Modifier le site, Ajouter des articles pour l'actualité ...</p>
+          <a href="#" class="btn btn-primary">Gérer le site</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+
+<!--Debut reflexion alerte stock article-->
+<div class="Infos Article" style="border:1px solid gray;">
+  <h3>Stocks des Articles</h3>
+  <?php foreach ($lesarticles as $article) {
+    if ($article->getQuantite() <= 4){
+      $nom = $article->getDescription();
+      $stock = $article->getQuantite();
+      echo "<p>$nom</p>";
+      echo "<p>Stock restant : $stock</p>";
+    }
+  } ?>
+</div>
+<script>
+function Article(id){
+  if(confirm("Voulez vous vraiment supprimer cet adhérent ?")){
+    window.location='../../controler/tableadherent/tableadherent.ctrl.php?type=delete&idAdherent='+id
+  }
+}
+</script>
+<!--Debut reflexion alerte stock article-->
+
   <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
