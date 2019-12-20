62c36eab39569789d113699ffb771337cdb94b42
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-20 14:03:53 +0100

    ckeditor

---
+++ view/autres/ajouterarticles.view.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<title>Insertion</title>
+		<script	src="../ckeditor/ckeditor.js"></script>
+	</head>
+	<body>
+		<h1>Insertion d'un nouvel article</h1>
+		<form action="index.php" method="post">
+			<textarea name="editeur" id="editeur" rows="8" cols="80"></textarea>
+			<input type="submit" value="envoi">
+		</form>
+		<script>
+		CKEDITOR.replace('editeur');
+		</script>
+	</body>
+</html>
---
+++ view/autres/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+     $editor_data = $_POST["editeur"];
+     echo $editor_data;
+    ?>
+  </body>
+</html>
