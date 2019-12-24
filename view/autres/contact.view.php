63a27ce41e60afdd11f5395843283032ef21efbf
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-24 14:25:58 +0100

    formulaire de contact

--- view/autres/contact.view.php
+++ view/autres/contact.view.php
@@ -4,7 +4,8 @@
   <!-- Required meta tags -->
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+  <link rel="stylesheet" href="../../view/css/main.view.css">
+  <link rel="stylesheet" href="../../view/css/form.view.css">
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
@@ -15,157 +16,79 @@
     <!--Image bandeau haut-->
     <div class="boximg">
       <p class="box">
-        <img class="scroll" src="../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
-        <img class="scroll" src="../model/data/images/images_sites/fond_banniere.jpg" alt="" style="display:none;">
-        <img class="scroll" src="../model/data/images/images_sites/ob_763433_p1620864.jpg" alt="" style="display:none;">
+        <img class="scroll" src="../../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
       </p>
     </div>
-    <!--Script defilement voir après.
-    <script type="text/javascript">
-    I = 0 ;
-    Imax = document.images.length - 1 ;
-    setTimeout(suivante, 7000) ;
-    function suivante()
-    {
-    document.images[I].style.display = "none" ;
-    if ( I < Imax )
-    I++;
-    else
-    I=0;
-    document.images[I].style.display = "block";
-    setTimeout(suivante, 7000) ;
-  }
-</script>
-<!Nav-->
-<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
-  <button class="navbar-toggler" data-toggle = "collapse" data-target = "#collapse_target">
-      <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="collapse_target">
-  <a class="navbar-brand" href="../controler/accueil.ctrl.php"> <img src="../model/data/images/images_sites/logo-scale.jpg" alt="logo scale Echirolles" width="60%"></a>
+
+    <!--Nav-->
+    <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+      <button class="navbar-toggler" data-toggle = "collapse" data-target = "#collapse_target">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapse_target">
+        <a class="navbar-brand" href="../../controler/accueil.ctrl.php"> <img src="../../model/data/images/images_sites/logo-scale.jpg" alt="logo scale Echirolles" width="60%"></a>
 
 
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" href="../controler/accueil.ctrl.php">Accueil</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../controler/actualite/touteslesactualites.ctrl.php">Actualités</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../controler/lebureau.ctrl.php">Le bureau</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../controler/lescoureurs.ctrl.php">Les coureurs</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../controler/tablehistoireclub/tablehistoireclub.ctrl.php">Histoire du club</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="../view/autres/contact.view.php">Nous contacter</a>
-      </li>
-    </ul>
-  </div>
-
-</nav>
-<!--Nav-->
-</header>
-<!--Fin header-->
-
-
-  <!--Section: Contact v.2-->
-  <section class="mb-4">
-
-      <!--Section heading-->
-      <h2 class="h1-responsive font-weight-bold text-center my-4">Contact us</h2>
-      <!--Section description-->
-      <p class="text-center w-responsive mx-auto mb-5">Vous avez une question ? N'hésitez pas à nous contacter !</p>
-      <div class="row">
-          <!--Grid column-->
-          <div class="col-md-9 mb-md-0 mb-5">
-              <form id="contact-form" name="contact-form" action="mail.php" method="POST">
-
-                  <!--Grid row-->
-                  <div class="row">
-
-                      <!--Grid column-->
-                      <div class="col-md-6">
-                          <div class="md-form mb-0">
-                              <input type="text" id="name" name="name" class="form-control">
-                              <label for="name" class="">Votre nom</label>
-                          </div>
-                      </div>
-                      <!--Grid column-->
-
-                      <!--Grid column-->
-                      <div class="col-md-6">
-                          <div class="md-form mb-0">
-                              <input type="text" id="email" name="email" class="form-control">
-                              <label for="email" class="">Votre email</label>
-                          </div>
-                      </div>
-                      <!--Grid column-->
-
-                  </div>
-                  <!--Grid row-->
-
-                  <!--Grid row-->
-                  <div class="row">
-                      <div class="col-md-12">
-                          <div class="md-form mb-0">
-                              <input type="text" id="subject" name="subject" class="form-control">
-                              <label for="subject" class="">Sujet</label>
-                          </div>
-                      </div>
-                  </div>
-                  <!--Grid row-->
-
-                  <!--Grid row-->
-                  <div class="row">
-
-                      <!--Grid column-->
-                      <div class="col-md-12">
-
-                          <div class="md-form">
-                              <textarea type="text" id="message" name="message" rows="2" class="form-control md-textarea"></textarea>
-                              <label for="message">Votre message</label>
-                          </div>
-
-                      </div>
-                  </div>
-                  <!--Grid row-->
-
-              </form>
-
-              <div class="text-center text-md-left">
-                  <a class="btn btn-primary" onclick="document.getElementById('contact-form').submit();">Envoyer</a>
-              </div>
-              <div class="status"></div>
-          </div>
-          <!--Grid column-->
-
-          <!--Grid column-->
-          <div class="col-md-3 text-center">
-              <ul class="list-unstyled mb-0">
-                  <li><i class="fas fa-map-marker-alt fa-2x"></i>
-                      <p>San Francisco, CA 94126, USA</p>
-                  </li>
-
-                  <li><i class="fas fa-phone mt-4 fa-2x"></i>
-                      <p>+ 01 234 567 89</p>
-                  </li>
-
-                  <li><i class="fas fa-envelope mt-4 fa-2x"></i>
-                      <p>contact@mdbootstrap.com</p>
-                  </li>
-              </ul>
-          </div>
-          <!--Grid column-->
-
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="../../controler/accueil.ctrl.php">Accueil</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../controler/actualite/touteslesactualites.ctrl.php">Actualités</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../controler/lebureau.ctrl.php">Le bureau</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../controler/lescoureurs.ctrl.php">Les coureurs</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../controler/tablehistoireclub/tablehistoireclub.ctrl.php">Histoire du club</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="../../view/autres/contact.view.php">Nous contacter</a>
+          </li>
+        </ul>
       </div>
 
-  </section>
-  <!--Section: Contact v.2-->
+    </nav>
+    <!--Nav-->
+  </header>
+  <!--Fin header-->
+
+
+
+  <h2>Contactez Nous</h2>
+
+  <p>Vous avez une question ? N'hésitez pas à nous contacter !</p>
+
+
+      <form class="" action="../../controler/contact.ctrl.php" method="post" style="width:35em;">
+        <p>
+            Votre nom :<br />
+           <input type="text" name="nom" value="" style="width:20em;"  />
+        </p>
+        <p>
+            Votre email :<br />
+           <input type="text" name="email" value="" style="width:20em;" />
+        </p>
+        <p>
+           Sujet :<br />
+           <input type="text" name="sujet" value="" style="width:20em;"/>
+        </p>
+        <p>
+           Votre message :<br />
+           <textarea name="message" rows="5" cols="80" style="width:30em;"></textarea>
+        </p>
+
+        <p>
+           <input type="submit" value="Envoyer" />
+           <input type="reset" value="Effacer" />
+           </p>
+      </form>
+
+
+
 
 
   <!-- Optional JavaScript -->
