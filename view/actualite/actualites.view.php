459811bb7f828b2132144b217cd67560555dfb75
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-24 13:40:33 +0100

    correction

--- view/actualite/actualites.view.php
+++ view/actualite/actualites.view.php
@@ -7,6 +7,7 @@
   <!-- Bootstrap CSS -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link href="../../view/css/blog.view.css" rel="stylesheet">
+
   <title>SCALE Echirolles - club de cyclisme</title>
 </head>
 <body>
@@ -15,62 +16,39 @@
     <!--Image bandeau haut-->
     <div class="boximg">
       <p class="box">
-        <img class="scroll" src="../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
-        <img class="scroll" src="../model/data/images/images_sites/fond_banniere.jpg" alt="" style="display:none;">
-        <img class="scroll" src="../model/data/images/images_sites/ob_763433_p1620864.jpg" alt="" style="display:none;">
+        <img class="scroll" src="../../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
       </p>
     </div>
-    <!--Script defilement voir après.
-    <script type="text/javascript">
-    I = 0 ;
-    Imax = document.images.length - 1 ;
-    setTimeout(suivante, 7000) ;
-    function suivante()
-    {
-    document.images[I].style.display = "none" ;
-    if ( I < Imax )
-    I++;
-    else
-    I=0;
-    document.images[I].style.display = "block";
-    setTimeout(suivante, 7000) ;
-  }
-</script>
-<!Nav-->
+
+<!--Nav-->
 <nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
   <button class="navbar-toggler" data-toggle = "collapse" data-target = "#collapse_target">
       <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="collapse_target">
-  <a class="navbar-brand" href="../controler/accueil.ctrl.php"> <img src="../model/data/images/images_sites/logo-scale.jpg" alt="logo scale Echirolles" width="60%"></a>
+  <a class="navbar-brand" href="../../controler/accueil.ctrl.php"> <img src="../../model/data/images/images_sites/logo-scale.jpg" alt="logo scale Echirolles" width="60%"></a>
 
 
     <ul class="navbar-nav">
       <li class="nav-item">
-        <a class="nav-link" href="../controler/accueil.ctrl.php">Accueil</a>
+        <a class="nav-link" href="../../controler/accueil.ctrl.php">Accueil</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../controler/actualite/touteslesactualites.ctrl.php">Actualités</a>
+        <a class="nav-link" href="../../controler/actualite/touteslesactualites.ctrl.php">Actualités</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../controler/lebureau.ctrl.php">Le bureau</a>
+        <a class="nav-link" href="../../controler/lebureau.ctrl.php">Le bureau</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../controler/lescoureurs.ctrl.php">Les coureurs</a>
+        <a class="nav-link" href="../../controler/lescoureurs.ctrl.php">Les coureurs</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../controler/tablehistoireclub/tablehistoireclub.ctrl.php">Histoire du club</a>
+        <a class="nav-link" href="../../controler/tablehistoireclub/tablehistoireclub.ctrl.php">Histoire du club</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link" href="../view/autres/contact.view.php">Nous contacter</a>
+        <a class="nav-link" href="../../view/autres/contact.view.php">Nous contacter</a>
       </li>
     </ul>
-    <form action="../controler/auth/login.ctrl.php" method="get" class="ml-auto">
-      <div class="input-group mb-3">
-
-        <button class="btn btn-success my-2 my-sm-0" type="submit"><?php echo $etat; ?></button>
-      </div>
-    </form>
   </div>
 
 </nav>
