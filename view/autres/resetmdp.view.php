38a74886571d3ff210ddb3283393243267098c55
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-24 12:00:06 +0100

    changement de toutes les en-tetes

--- view/autres/resetmdp.view.php
+++ view/autres/resetmdp.view.php
@@ -11,22 +11,24 @@
 
   <title>SCALE Echirolles - club de cyclisme</title>
 </head>
-<body style="background-color:silver;">
+<body>
 
-  <div class="boximg" style="margin-bottom:0">
-    <p class="box">
-      <img class="scroll" src="../../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
-      <img class="scroll" src="../../model/data/images/images_sites/fond_banniere.jpg" alt="" style="display:none;">
-      <img class="scroll" src="../../model/data/images/images_sites/ob_763433_p1620864.jpg" alt="" style="display:none;">
-    </p>
-  </div>
-
-  <script type="text/javascript">
-  I = 0 ;
-  Imax = document.images.length - 1 ;
-  setTimeout(suivante, 7000) ;
-  function suivante()
-  {
+  <header>
+    <!--Image bandeau haut-->
+    <div class="boximg">
+      <p class="box">
+        <img class="scroll" src="../model/data/images/images_sites/accueil_banniere.jpg" alt="" style="display:block; vertical-align:middle;">
+        <img class="scroll" src="../model/data/images/images_sites/fond_banniere.jpg" alt="" style="display:none;">
+        <img class="scroll" src="../model/data/images/images_sites/ob_763433_p1620864.jpg" alt="" style="display:none;">
+      </p>
+    </div>
+    <!--Script defilement voir après.
+    <script type="text/javascript">
+    I = 0 ;
+    Imax = document.images.length - 1 ;
+    setTimeout(suivante, 7000) ;
+    function suivante()
+    {
     document.images[I].style.display = "none" ;
     if ( I < Imax )
     I++;
@@ -35,37 +37,49 @@
     document.images[I].style.display = "block";
     setTimeout(suivante, 7000) ;
   }
-  </script>
+</script>
+<!Nav-->
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark sticky-top">
+  <button class="navbar-toggler" data-toggle = "collapse" data-target = "#collapse_target">
+      <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapse_target">
+  <a class="navbar-brand" href="../controler/accueil.ctrl.php"> <img src="../model/data/images/images_sites/logo-scale.jpg" alt="logo scale Echirolles" width="60%"></a>
 
-  <nav class="navbar navbar-expand-sm bg-dark navbar-dark" style="display:flex; flex-wrap:wrap;">
-    <a class="navbar-brand" href="#"> <img src="../../model/data/images/images_sites/logo-scale.jpg" alt="" width="60%"></a>
 
-    <div class="collapse navbar-collapse" id="collapsibleNavbar">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link" href="main.view.php">Accueil</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="actualites.view.php">Actualités</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="lebureau.view.php">Le bureau</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../../controler/lescoureurs.ctrl.php">Les coureurs</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="histoireclub.view.php">Histoire du club</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="contact.view.php">Nous contacter</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="../../view/page administrateur/admin.view.php">Page administrateur</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="../controler/accueil.ctrl.php">Accueil</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../controler/actualite/touteslesactualites.ctrl.php">Actualités</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../controler/lebureau.ctrl.php">Le bureau</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../controler/lescoureurs.ctrl.php">Les coureurs</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../controler/tablehistoireclub/tablehistoireclub.ctrl.php">Histoire du club</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="../view/autres/contact.view.php">Nous contacter</a>
+      </li>
+    </ul>
+    <form action="../controler/auth/login.ctrl.php" method="get" class="ml-auto">
+      <div class="input-group mb-3">
+
+        <button class="btn btn-success my-2 my-sm-0" type="submit"><?php echo $etat; ?></button>
+      </div>
+    </form>
+  </div>
+
+</nav>
+<!--Nav-->
+</header>
+<!--Fin header-->
+
   <div class="container">
     <div class="d-flex justify-content-center h-100">
       <div class="card">
