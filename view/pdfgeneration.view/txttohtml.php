b7588d35f7dc7c784ca56b9c918e61806f33c21b
remulef <fabienremule974@gmail.com>
2019-12-28 19:42:36 +0100

    func

--- view/pdfgeneration.view/txttohtml.php
+++ view/pdfgeneration.view/txttohtml.php
@@ -1,6 +1,7 @@
 <?php
 
-public function txt2html($chemin,$nomfichier,$cheminformulaire):int
+
+public function txt2html($chemin='exemple.txt',$nomfichier='exemple.txt',$cheminformulaire='chemin_formulaire',$nomformulaire='nom_formulaire'):int
 {
   if (fopen( $chemin,"r")) {
    $in=fopen( $chemin, "r" );
@@ -11,29 +12,40 @@ public function txt2html($chemin,$nomfichier,$cheminformulaire):int
   //$out= fopen( $nomfichier,"x");
 
   $file = new SplFileObject("$nomfichier");
-echo '<div id="formulaire">
-<form class="" action="../../model/fpdm/ex-array.php" method="post" autocomplete="off">
-<p> Name :<br />
-<input list="nom" type="text" name="name" placeholder="Name par defaut "/>
-<datalist id="nom">
-<option value="proposition nom 1 ">
-<option value="proposition nom 2">
-<option value="proposition nom 3">
-<option value="proposition nom 4">
-</datalist>
-</p>';
+
+
+//Crée le formulaire avec le chemin vers le controleur adapté
+printf('<div id="%s">
+<form class="" action="../../controleur/pdf/%s.php" method="post" autocomplete="on
+">',$nomformulaire);
+
+
   while (!$file->eof()) {
-    $ligne_courante = fgets($in);
+
     if (strpos($ligne_courante,'FieldName')) {
-
-      $ligne_courante = explode('FieldName: ',$ligne_courante);
-
+      //Methode generique pour du text
+      $ligne_courante = explode('FieldName:',$ligne_courante);
+      $txt = strtolower($ligne_courante);
+      printf('<p>%s :<br />
+      <input list="%s" type="text" name="%s"  />
+      </p>',$ligne_courante,$ligne_courante,$txt);
+      // implementer un if pour créer des checkbox
+      //Mais a convertir dans le controleur pour transformer en 'X'
     }
+    $ligne_courante = fgets($in);
   }
 
-
+echo ' <div class="Bouton">
+  <INPUT TYPE="submit" NAME="valider" VALUE="Valider">
+  <INPUT TYPE="reset" NAME="reset" VALUE="Reinitialiser">
+  </div>
+  </form>';
 
 }
+
+//test
+txt2html()
+
 
 
 
