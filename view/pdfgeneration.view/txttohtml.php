f904ccc8015ccae2422d0ec0315bb8f99c470abd
remulef <fabienremule974@gmail.com>
2019-12-28 18:04:59 +0100

    Update txttohtml.php

--- view/pdfgeneration.view/txttohtml.php
+++ view/pdfgeneration.view/txttohtml.php
@@ -1,6 +1,6 @@
 <?php
 
-public function txt2html($chemin,$nomfichier):int
+public function txt2html($chemin,$nomfichier,$cheminformulaire):int
 {
   if (fopen( $chemin,"r")) {
    $in=fopen( $chemin, "r" );
@@ -8,16 +8,26 @@ public function txt2html($chemin,$nomfichier):int
    echo "i marche pas PD";
    return -1;
  }
-  $out= fopen( $nomfichier,"x");
+  //$out= fopen( $nomfichier,"x");
 
   $file = new SplFileObject("$nomfichier");
-
+echo '<div id="formulaire">
+<form class="" action="../../model/fpdm/ex-array.php" method="post" autocomplete="off">
+<p> Name :<br />
+<input list="nom" type="text" name="name" placeholder="Name par defaut "/>
+<datalist id="nom">
+<option value="proposition nom 1 ">
+<option value="proposition nom 2">
+<option value="proposition nom 3">
+<option value="proposition nom 4">
+</datalist>
+</p>';
   while (!$file->eof()) {
     $ligne_courante = fgets($in);
     if (strpos($ligne_courante,'FieldName')) {
 
       $ligne_courante = explode('FieldName: ',$ligne_courante);
-    printf('')
+
     }
   }
 
