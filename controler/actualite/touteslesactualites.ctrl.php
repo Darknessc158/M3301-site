8196613b70e77cc78dcbaa242cb0e11ee08b6761
Momiouo <47118225+Momiouo@users.noreply.github.com>
2019-12-30 12:12:42 +0100

    correction et ajout mineurs

--- controler/actualite/touteslesactualites.ctrl.php
+++ controler/actualite/touteslesactualites.ctrl.php
@@ -2,28 +2,29 @@
 require_once('../../model/classPublication/publication.class.php');
 require_once('../../model/classPublication/publicationDAO.class.php');
 
-$idPage = 1;
+
 
 $publications = new publicationDAO();
 
 $lespublications = $publications->getLesArticlesBlog();
 
-if (isset($_GET['page'])){ //pagination blog
-  $idPage = $_GET['page'];
-}
-if (isset($_GET['action'])) {
+if (isset($_GET['action'])) { //gestion numero de la page
   $action = $_GET['action'];
   if ($action == 'next') {
-    $idPage = $idPage+1;
+    $idPage = $idPage++;
   }
   if ($action == 'prev') {
     if ($idPage <= 1){
       $idPage = 1;
     }else{
-      $idPage = $idPage-1;
+      $idPage = $idPage--;
     }
   }
+}else{
+  $idPage = 1;
 }
+
+//gestion publication a afficher en fonction de la page
 $start = ($idPage-1)*4;
 $end = ($idPage*4)-1;
 if (array_key_exists($end,$lespublications)){
@@ -32,12 +33,23 @@ if (array_key_exists($end,$lespublications)){
   while (!array_key_exists($end,$lespublications)) {
     $end--;
   }
-  if ($end == 0) {
-    echo "Plus d'articles";
-  }else{
+  if (($end != 0) && ($end >= $start)) {
     $lespublications = $publications->pagination($start,$end);
+  }else{ // on recalcul avec l'idPage d'avant
+    $idPage =$idPage--;
+    $start = ($idPage-1)*4;//on recalcul
+    $end = ($idPage*4)-1;
+    if (array_key_exists($end,$lespublications)){
+      $lespublications = $publications->pagination($start,$end);
+    }else {
+      while (!array_key_exists($end,$lespublications)) {
+        $end--;
+      }
+      if (($end != 0) && ($end >= $start)) {
+        $lespublications = $publications->pagination($start,$end);
+      }
+    }
   }
-}
 
 
 
