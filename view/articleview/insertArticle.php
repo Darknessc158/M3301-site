9b078063034dc8eeee817de2b40b97ec417bb4d6
Momiouo <47118225+Momiouo@users.noreply.github.com>
2020-01-11 13:42:58 +0100

    eaz
    azeaaze

--- view/articleview/insertArticle.php
+++ view/articleview/insertArticle.php
@@ -9,36 +9,35 @@
   <h1>Ajout d'un produit</h1>
   <form class="" action="../../controler/tablearticle/insertArticle.ctrl.php" method="post">
     <p>
-        Description (Nom, référence, taille ...) :<br />
-       <input type="text" name="description" value="" />
+      Description (Nom, référence, taille ...) :<br />
+      <input type="text" name="description" value="" />
     </p>
     <p>
-        Prix :<br />
-       <input type="text" name="prix" value="" />
+      Prix :<br />
+      <input type="text" name="prix" value="" />
     </p>
     <p>
-       Catégorie(pneu,tenue...) :<br />
-       <input type="text" name="categorie" value="" />
+      Catégorie(pneu,tenue...) :<br />
+      <input type="text" name="categorie" value="" />
     </p>
     <p>
-       Quantité en stock :<br />
-       <input type="text" name="stock" value="" />
+      Quantité en stock :<br />
+      <input type="text" name="stock" value="" />
     </p>
     <p>
-       Marque :<br />
-       <input type="text" name="marque" value="" />
+      Marque :<br />
+      <input type="text" name="marque" value="" />
     </p>
 
     <p>
-       <input type="submit" value="Ajouter" />
-       <input type="reset" value="Annuler" />
-       </p>
-       <p>
-       Retour : <a id="retour" href="../../controler/tablearticle/tablearticle.ctrl.php"><i class="fas fa-undo"></i></a>
-    </p>
-  </form>
+      <input type="submit" value="Ajouter" />
+      <input type="reset" value="Annuler" />
+      <p>
+        Retour : <a id="retour" href="../../controler/tablearticle/tablearticle.ctrl.php"><i class="fas fa-undo"></i></a>
+      </p>
+    </form>
 
 
-</body>
-<script src="https://kit.fontawesome.com/a076d05399.js"></script>
-</html>
+  </body>
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </html>
